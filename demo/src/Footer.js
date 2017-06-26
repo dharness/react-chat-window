45d815ea158f0beed4dd153a21c218876a374536
Dylan Harness <dharness@uwo.ca>
2017-06-25 23:19:47 -0400

    added animation to logo

---
+++ demo/src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render () {
+    return (
+      <div className="demo-footer">
+        
+      </div>
+    )
+  }
+}
+
+export default Footer
--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import ChatWindow from './ChatWindow';
+import launcherIcon from './../assets/logo-no-bg.svg';
+import launcherIconActive from './../assets/close-icon.png';
 
 
 class Launcher extends Component {
 
   constructor() {
     super();
-    this.state = { active: false };
+    this.state = {
+      active: false,
+      launcherIcon
+    };
   }
 
   handleClick() {
-    this.setState({ active: !this.state.active });
+    this.setState({
+      active: !this.state.active
+    });
   }
 
   render() {
@@ -21,7 +28,10 @@ class Launcher extends Component {
     ];
     return (
       <div>
-        <div className={classList.join(' ')} onClick={this.handleClick.bind(this)} />
+        <div className={classList.join(' ')} onClick={this.handleClick.bind(this)}>
+          <img className={"sc-open-icon"} src={launcherIconActive} />
+          <img className={"sc-closed-icon"} src={launcherIcon} />
+        </div>
         <ChatWindow
           messageList={this.props.messageList}
           onUserInputSubmit={this.props.onMessageWasSent}
