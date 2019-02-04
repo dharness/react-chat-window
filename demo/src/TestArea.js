f6cec81c6878248febdf5bf3febe36d7e9616096
Heather Booker <heather.booker@samsung.com>
2019-02-04 14:04:38 -0500

    Fix date and link text for demo

--- demo/src/TestArea.js
+++ demo/src/TestArea.js
@@ -25,7 +25,7 @@ class TestArea extends Component {
           react-chat-window is a chat window that allows you to build and add custom live chat to your sites. It includes only the react chat widget. There is no backend, and no communication system baked in.
           <br />
           <br/>
-          For instructions on how to use react-chat-window click <a href="https://github.com/kingofthestack/react-chat-window">here</a>.
+          Usage instructions for react-chat-window are <a href="https://github.com/kingofthestack/react-chat-window">on Github</a>.
         </p>
       </div>
     )
