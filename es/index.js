98bb46a10450f43431649817eeea794cea3c8534
Dylan Harness <dharness@uwo.ca>
2019-03-30 11:25:50 -0400

    .

--- es/index.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import './styles';
-import Launcher from './components/Launcher';
-
-export { Launcher };
--- es/styles/index.js
+++
@@ -1,7 +0,0 @@
-import './emojiPicker.css';
-import './chat-window.css';
-import './launcher.css';
-import './header.css';
-import './message.css';
-import './user-input.css';
-import './popup-window.css';
