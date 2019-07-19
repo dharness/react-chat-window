bee451ab8db9adab78298e717d9eaf7675be8a44
Heather Booker <heather.booker@samsung.com>
2019-07-19 14:45:47 -0400

    Split out webpack config for dev vs prod

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: './demo/src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'demo/dist')
   },
   module: {
     rules: [{
