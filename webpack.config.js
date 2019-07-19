70f48f6cb0a8242ab6fcefa73cc89abf59722c5e
Heather Booker <heather.booker@samsung.com>
2019-07-19 11:23:14 -0400

    Update package.json - move all non-dependency fields to top - move webpack etc to devDependencies - add keywords - set webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
